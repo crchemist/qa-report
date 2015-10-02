dfdd5a041610ce3f1a9b54fca301d406e02a31d3
Mykola Kharechko <crchemist@gmail.com>
2015-10-02 15:32:46 +0300

    added devtools panel.

---
+++ resources/js/devtools.js
@@ -0,0 +1,7 @@
+console.log('hiiiii!!')
+chrome.devtools.panels.create("QA Report",
+    "icon48.png",
+        "devtools-panel.html",
+            function(panel) {
+                }
+                );
