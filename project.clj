187501020698e775ad35fff34c2da71143941b94
Mykola Kharechko <crchemist@gmail.com>
2015-10-06 17:57:17 +0300

    updated versions.

--- project.clj
+++ project.clj
@@ -1,14 +1,15 @@
 (defproject qa-report "0.1.0-SNAPSHOT"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]
-                 [org.clojure/clojurescript "0.0-2156"]
-                 [org.clojure/core.async "0.1.242.0-44b1e3-alpha"]
+  :dependencies [[org.clojure/clojure "1.7.0"]
+                 [org.clojure/clojurescript "1.7.48"]
+                 [org.clojure/core.async "0.1.346.0-17112a-alpha"]
                  [khroma "0.0.2"]
                  [prismatic/dommy "0.1.2"]]
   :source-paths ["src"]
   :profiles {:dev {:plugins [[com.cemerick/austin "0.1.3"]
-                             [lein-cljsbuild "1.0.1"]
+                             [lein-cljsbuild "1.1.0"]
+                             [lein-git-deps "0.0.1-SNAPSHOT"]
                              [lein-chromebuild "0.2.1"]]
 
                    :cljsbuild {:builds {:main
--- resources/js/devtools.js
+++
@@ -1,7 +0,0 @@
-console.log('hiiiii!!')
-chrome.devtools.panels.create("QA Report",
-    "icon48.png",
-        "devtools-panel.html",
-            function(panel) {
-                }
-                );
